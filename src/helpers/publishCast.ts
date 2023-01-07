aaac83fb596ddbcb3db14266baba5cc09c891f72
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-01-06 17:32:59 -0800

    bring back publishing

--- src/helpers/publishCast.ts
+++ src/helpers/publishCast.ts
@@ -6,6 +6,6 @@ const client = new MerkleAPIClient(wallet)
 export default async function (text: string, replyToId: string) {
   const cast = await client.fetchCast(replyToId)
   console.log('Publishing cast', text, cast?.hash)
-  // const publishedCast = await client.publishCast(text, cast)
-  // return publishedCast.hash
+  const publishedCast = await client.publishCast(text, cast)
+  return publishedCast.hash
 }
